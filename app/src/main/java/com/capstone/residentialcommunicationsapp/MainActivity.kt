14f2a68628db96e32d2fc70ad3db47d109336545
Erica Travers <etravers@student.bridgew.edu>
2020-02-22 11:42:39 -0500

    Add activities and auth file

--- app/src/main/java/com/capstone/residentialcommunicationsapp/MainActivity.kt
+++ app/src/main/java/com/capstone/residentialcommunicationsapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.capstone.residentialcommunicationsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val propertyPortalBtn = findViewById<Button>(R.id.propertyPortalBtn)
+        propertyPortalBtn.setOnClickListener {
+                val intent = Intent(this, PropertyPortalActivity::class.java)
+                startActivity(intent)
+        }
+
+        val tenantPortalBtn = findViewById<Button>(R.id.tenantPortalBtn)
+                tenantPortalBtn.setOnClickListener {
+                    val intent = Intent(this, TenantPortalActivity::class.java)
+                    startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/capstone/residentialcommunicationsapp/PropertyPortalActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.residentialcommunicationsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PropertyPortalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property_portal)
+    }
+}
