39665b21a0d30e573cae776d83f30f4e7c3b9cf5
Erica Travers <etravers@student.bridgew.edu>
2020-03-02 17:40:53 -0500

    Add activities for login screen and home screens

---
+++ app/src/main/java/com/capstone/residentialcommunicationsapp/Authentication.kt
@@ -0,0 +1,10 @@
+package com.capstone.residentialcommunicationsapp
+
+class Authentication() {
+    /*val sharedPref = PropertyPortalActivity?.getPreference(Context.MODE_PRIVATE) ?: return
+    with (sharedPref.edit())
+    {
+        putInt(getString(R.string.saved_high_score_key), newHighScore)
+        commit()
+    }*/
+}
