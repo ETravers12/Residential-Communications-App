cb5be00727c1db194d6125cb7ecaaef885ebc61d
Erica Travers <etravers@student.bridgew.edu>
2020-04-05 21:38:58 -0400

    Move property manager connection to the next activity

--- app/src/main/java/com/capstone/residentialcommunicationsapp/PropertyHomeScreen.kt
+++ app/src/main/java/com/capstone/residentialcommunicationsapp/PropertyHomeScreen.kt
@@ -3,13 +3,28 @@ package com.capstone.residentialcommunicationsapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.capstone.residentialcommunicationsapp.datamodels.PropertyManager
+import com.capstone.residentialcommunicationsapp.datamodels.PropertyManagerViewModel
 
 class PropertyHomeScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_home_screen)
+
+        val model: PropertyManagerViewModel by viewModels()
+        model.fetchPropertyManagerUsers();
+
+        val placeholder = findViewById<TextView>(R.id.prop_home_text)
+
+        placeholder.text = "Loading...";
+        model.propertyManagerUsersLiveData.observe(this, Observer<List<PropertyManager>>{ users ->
+            placeholder.text = "property user " + users[0].propertyManagerName + " retrieved"
+        })
 
         val propCheckIssuesBtn = findViewById<Button>(R.id.propCheckIssuesBtn)
         propCheckIssuesBtn.setOnClickListener {
--- app/src/main/java/com/capstone/residentialcommunicationsapp/PropertyPortalActivity.kt
+++ app/src/main/java/com/capstone/residentialcommunicationsapp/PropertyPortalActivity.kt
@@ -3,28 +3,13 @@ package com.capstone.residentialcommunicationsapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import com.capstone.residentialcommunicationsapp.datamodels.PropertyManager
-import com.capstone.residentialcommunicationsapp.datamodels.PropertyManagerViewModel
 
 class PropertyPortalActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_portal)
-
-        val model: PropertyManagerViewModel by viewModels()
-        model.fetchPropertyManagerUsers();
-
-        val placeholder = findViewById<TextView>(R.id.prop_home_text)
-
-        placeholder.text = "Loading...";
-        model.propertyManagerUsersLiveData.observe(this, Observer<List<PropertyManager>>{users ->
-            placeholder.text = "property user " + users[0].propertyManagerName + " retrieved"
-        })
 
         val propLoginSubmit = findViewById<Button>(R.id.propLoginSubmit)
         propLoginSubmit.setOnClickListener {
